1d66b8f09d0ff1497187c0901e12623bd770a53a
kasahara-t <kasahara.tomoya.dev@gmail.com>
2024-05-08 00:34:23 +0900

    refactor: 💄 ログ表示の余白を調整

--- src/components/game/LogPanel.tsx
+++ src/components/game/LogPanel.tsx
@@ -12,11 +12,18 @@ export const LogPanel: FC = () => {
         "bg-black bg-opacity-30 shadow-custom",
       )}
     >
-      <h3 className={cn("text-3xl flex mt-6 justify-center items-center")}>
+      <h3 className={cn("text-3xl flex my-6 justify-center items-center")}>
         Log
       </h3>
       {gameLogs.map((log, i) => (
-        <p key={i}>{log}</p>
+        <p
+          key={i}
+          className={cn("py-2", {
+            "border-t-2 border-white border-opacity-10": i > 0,
+          })}
+        >
+          {log}
+        </p>
       ))}
     </div>
   );
