11eba0e5408877ac66febe5d52f88c675b491866
kasahara-t <kasahara.tomoya.dev@gmail.com>
2024-05-10 12:23:01 +0900

    test: :sparkles: bunのテストランナーを使ってinitializeBoardのテストを実装

---
+++ src/game/logics/boardLogic.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect, test } from "bun:test";
+import type { BoardSize } from "../types/board";
+import { initializeBoard } from "./boardLogic";
+
+describe("initializeBoard", () => {
+  const boardSizes: [BoardSize, number][] = [
+    [3, 9],
+    [6, 36],
+  ];
+  test.each(boardSizes)(
+    "サイズが%pで初期化されたボードには%p個のタイルが生成される",
+    (size, expected) => {
+      const board = initializeBoard(size as BoardSize);
+      expect(board.tiles).toHaveLength(expected as number);
+    },
+  );
+});
