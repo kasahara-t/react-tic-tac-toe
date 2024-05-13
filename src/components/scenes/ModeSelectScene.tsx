15daac0049257441b914bdb7ed25f6a753a1d356
kasahara-t <kasahara.tomoya.dev@gmail.com>
2024-05-14 02:54:03 +0900

    feat: ✨ i18next-scannerを導入

--- src/components/scenes/ModeSelectScene.tsx
+++ src/components/scenes/ModeSelectScene.tsx
@@ -3,12 +3,14 @@ import { playersStateAtom } from "@/game/stores/atoms";
 import { cn } from "@/lib/utils";
 import { useAtom } from "jotai";
 import type { FC } from "react";
+import { useTranslation } from "react-i18next";
 import { AppBackground } from "../ui/AppBackground";
 import { Button } from "../ui/Button";
 
 export const ModeSelectScene: FC = () => {
   const { goToGame } = useScene();
   const [, setPlayers] = useAtom(playersStateAtom);
+  const { t } = useTranslation();
 
   const handleModeChange = (mode: "single" | "multi") => () => {
     if (mode === "single") {
@@ -39,10 +41,10 @@ export const ModeSelectScene: FC = () => {
         <h1 className={cn("text-4xl")}>Select a mode</h1>
         <div className={cn("flex flex-col gap-2")}>
           <Button variant="panel" onClick={handleModeChange("single")}>
-            Single player
+            {t("SinglePlay")}
           </Button>
           <Button variant="panel" onClick={handleModeChange("multi")}>
-            Multi player
+            {t("MultiPlay")}
           </Button>
         </div>
       </div>
