d24178234774a03e9a1c02ffe8ce88ee56fe1d82
kasahara-t <kasahara.tomoya.dev@gmail.com>
2024-05-14 22:37:44 +0900

    perf: ⚡️ プレイヤー名をi18n対応

--- src/game/hooks/useUpdatePlayers.ts
+++ src/game/hooks/useUpdatePlayers.ts
@@ -1,26 +1,34 @@
 import { useAtom } from "jotai";
+import { useTranslation } from "react-i18next";
 import { playersStateAtom } from "../stores/atoms";
 
 export const useUpdatePlayers = () => {
   const [, setPlayers] = useAtom(playersStateAtom);
+  const { t } = useTranslation();
 
   const setSinglePlayer = (isPlayerOneCPU: boolean) => {
+    const CPUName = t("CPUName");
+    const humanName = t("HumanName");
+
     setPlayers({
       Player1: {
-        name: isPlayerOneCPU ? "CPU" : "Human",
+        name: isPlayerOneCPU ? CPUName : humanName,
         isCPU: isPlayerOneCPU,
       },
       Player2: {
-        name: !isPlayerOneCPU ? "CPU" : "Human",
+        name: !isPlayerOneCPU ? CPUName : humanName,
         isCPU: !isPlayerOneCPU,
       },
     });
   };
 
   const setMultiPlayer = () => {
+    const player1Name = t("Player1Name");
+    const player2Name = t("Player2Name");
+
     setPlayers({
-      Player1: { name: "Player 1", isCPU: false },
-      Player2: { name: "Player 2", isCPU: false },
+      Player1: { name: player1Name, isCPU: false },
+      Player2: { name: player2Name, isCPU: false },
     });
   };
 
