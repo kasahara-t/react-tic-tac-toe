4b6f5900e2b17263ed824d4c923a15eda4f489da
kasahara-t <kasahara.tomoya.dev@gmail.com>
2024-05-20 12:45:06 +0900

    refactor: :truck: appディレクトリ内を整理

--- packages/app/src/app/ui/SceneSwitcher.tsx
+++ packages/app/src/app/ui/SceneSwitcher.tsx
@@ -1,7 +1,7 @@
+import { GameScene } from "@/components/scenes/GameScene";
+import { ModeSelectScene } from "@/components/scenes/ModeSelectScene";
 import { useScene } from "@/game/hooks/useScene";
 import type { FC } from "react";
-import { GameScene } from "./GameScene";
-import { ModeSelectScene } from "./ModeSelectScene";
 
 export const SceneSwitcher: FC = () => {
   const { scene } = useScene();
