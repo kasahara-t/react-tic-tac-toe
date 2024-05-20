b57e856b3f345121a9f7bdd6f10ef4434630c669
kasahara-t <kasahara.tomoya.dev@gmail.com>
2024-05-20 11:52:02 +0900

    perf: :zap: .envファイルの値がセットされているかをビルド時に確認するように変更

--- packages/app/src/lib/env/client.ts
+++ packages/app/src/lib/env/client.ts
@@ -4,12 +4,12 @@ import { z } from "zod";
 export const env = createEnv({
   clientPrefix: "VITE_", // クライアント側で使う環境変数のプレフィックス
   client: {
-    VITE_FIREBASE_API_KEY: z.string(),
-    VITE_FIREBASE_AUTH_DOMAIN: z.string(),
-    VITE_FIREBASE_PROJECT_ID: z.string(),
-    VITE_FIREBASE_STORAGE_BUCKET: z.string(),
-    VITE_FIREBASE_MESSAGING_SENDER_ID: z.string(),
-    VITE_FIREBASE_APP_ID: z.string(),
+    VITE_FIREBASE_API_KEY: z.string().min(1),
+    VITE_FIREBASE_AUTH_DOMAIN: z.string().min(1),
+    VITE_FIREBASE_PROJECT_ID: z.string().min(1),
+    VITE_FIREBASE_STORAGE_BUCKET: z.string().min(1),
+    VITE_FIREBASE_MESSAGING_SENDER_ID: z.string().min(1),
+    VITE_FIREBASE_APP_ID: z.string().min(1),
     VITE_FIREBASE_MEASUREMENT_ID: z.string().optional(), // オプションの場合
   },
   runtimeEnv: import.meta.env,
